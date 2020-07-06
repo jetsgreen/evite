04240f91ce09d97737d9f35fb20d207d9ede9356
Felix Fernandez <fernandezfelix460.gmail.com>
2020-07-06 10:41:56 -0400

    set up files and styled main page

---
+++ main.js
@@ -0,0 +1,16 @@
+// var toggle = document.querySelector('.hamburger-menu');
+// var navbar =document.querySelector('ul');
+// var noShow = document.getElementById('title');
+
+
+// toggle.addEventListener('click', function(){
+//     navbar.classList.toggle('active');
+//     noShow.classList.toggle('disapear')
+// });
+
+
+
+
+
+
+
